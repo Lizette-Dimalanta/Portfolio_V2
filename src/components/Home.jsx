ec46e33e4dfa9ddc176f58965d009e3db0f64817
Lizette Dimalanta <105244120+Lizette-Dimalanta@users.noreply.github.com>
2023-05-18 20:10:45 +1000

    Changed Profile Photo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { AiOutlineArrowDown } from "react-icons/ai";
+import { AiOutlineArrowDown } from 'react-icons/ai';
 
 const Home = () => {
   return (
     <div className='flex border-black border-2 mt-3 relative'>
-        <img src='../images/ProfilePhoto.jpg' alt='Profile Image' className='w-1/2 h-auto items-start object-cover'/>
+        <img src='../images/ProfileImage.jpeg' alt='Profile Image' className='w-1/2 h-auto items-start object-cover'/>
         <div className='text-left font-Inter mt-5 ml-5 sm:text-3xl sm:mt-14 sm:ml-14 md:text-4xl md:m-20 lg:text-5xl lg:m-28 xl:text-5xl xl:m-36'>
           <p>Passion,</p>
           <p>Creativity,</p>
