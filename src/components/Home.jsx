538a46da69aa99a58fdbf911ef589227b44dace7
Lizette Dimalanta <105244120+Lizette-Dimalanta@users.noreply.github.com>
2023-05-19 20:43:53 +1000

    Changed border to gray, rounded

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,32 @@
 import React from 'react'
-import { AiOutlineArrowDown } from 'react-icons/ai';
+// import { AiOutlineArrowDown } from 'react-icons/ai';
 
 const Home = () => {
   return (
-    <div className='flex border-black border-2 mt-3 relative'>
-        <img src='../images/ProfileImage.jpeg' alt='Profile Image' className='w-1/2 h-auto items-start object-cover'/>
-        <div className='text-left font-Inter mt-5 ml-5 sm:text-3xl sm:mt-14 sm:ml-14 md:text-4xl md:m-20 lg:text-5xl lg:m-28 xl:text-5xl xl:m-36'>
-          <p>Passion,</p>
-          <p>Creativity,</p>
-          <p>Development,</p>
-          <p>Humility.</p>
-        </div>
-        <button className='font-Roboto-Mono absolute bottom-0 right-0 mb-4 mr-4 font-bold italic p-0 bg-white hover:scale-105 transition-all ease-in-out duration-300 hover:animate-pulse'>
-          Get to know me 
-          <AiOutlineArrowDown  className='inline ml-1' />
-        </button>
+    <div className='flex border-slate-200 rounded border-2 mt-3 relative'>
+      <img src='../images/ProfileImage.jpeg' alt='Profile Image' className=''/>
     </div>
   )
 }
 
 export default Home
+
+// const Home = () => {
+//   return (
+//     <div className='flex border-black border-2 mt-3 relative'>
+//         <img src='../images/ProfileImage.jpeg' alt='Profile Image' className='w-1/2 h-auto items-start object-cover'/>
+//         <div className='text-left font-Inter mt-5 ml-5 sm:text-3xl sm:mt-14 sm:ml-14 md:text-4xl md:m-20 lg:text-5xl lg:m-28 xl:text-5xl xl:m-36'>
+//           <p>Passion,</p>
+//           <p>Creativity,</p>
+//           <p>Development,</p>
+//           <p>Humility.</p>
+//         </div>
+//         <button className='font-Roboto-Mono sm:text-xs sm:ml-14 md:text-4xl absolute bottom-0 right-0 mb-4 mr-4 font-bold italic p-0 bg-white hover:scale-105 transition-all ease-in-out duration-300 hover:animate-pulse'>
+//           Get to know me 
+//           <AiOutlineArrowDown  className='inline ml-1' />
+//         </button>
+//     </div>
+//   )
+// }
+
+// export default Home
