e14ad99cf02360656e0cd98aa98615c59996c4a6
Lizette Dimalanta <105244120+Lizette-Dimalanta@users.noreply.github.com>
2023-06-04 15:33:00 +1000

    Updated background image path

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,10 @@ export default {
         'DM-Sherif-Display': ['DM Sherif Display', 'Times New Roman'],
         'Inter': ['Inter', 'Arial', 'Helvetica'],
         'Roboto-Mono': ['Roboto Mono', 'Monospace']
-      }
+      },
+      backgroundImage: {
+        'backgroundImage': "url('/images/ProfileImage.png')",
+      },
     },
   },
   plugins: [],
