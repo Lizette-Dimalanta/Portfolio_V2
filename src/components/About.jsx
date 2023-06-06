d2902962d0b957db0cf4acec9cda1e274f894d36
Lizette Dimalanta <105244120+Lizette-Dimalanta@users.noreply.github.com>
2023-06-06 22:44:26 +1000

    Added carousel images

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,54 +7,56 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css'
 const About = () => {
 
   return (
-    <div id='About' className='max-w-[1040px] m-auto px-5'>
-      <h1 className='font-DM-Sherif-Display italic font-semibold text-4xl text-slate-900 text-start underline underline-offset-8 py-10 sm:pl-1'>
-        ABOUT
-      </h1>
-      <p className='flex text-start font-Inter tracking-wide text-slate-800 text-md p-1 w-screen md:w-1/2 leading-loose pb-5 pr-10'>
-        A passionate, driven and explorative thinker - fuelled by curiosity in discovering innovative solutions through the combination of business, design, technological and humanitarian perspectives. 
-      </p>
-      <h2 className='font-DM-Sherif-Display italic font-semibold text-3xl text-slate-700 text-start py-3'>
-        Find out more:
-      </h2>
-
-      <Accordion allowZeroExpanded className='text-start w-screen md:w-1/2 pr-10'>
-        <AccordionItem>
-          <AccordionItemHeading>
-            <AccordionItemButton className='px-3 py-1 font-Roboto-Mono text-md border-[1px] border-slate-200 rounded-md shadow-sm'>
-              ▾ "How would your friends describe you?"
-            </AccordionItemButton>
-          </AccordionItemHeading>
-          <AccordionItemPanel className='px-4 py-2'>
-            <p className='text-lg italic'>
-              "Lizette is funny, hardworking and loveable. She makes a great partner in any project and always gives the task at hand 110%."
-            </p>
-            <p className='font-Inter text-sm tracking-wide text-slate-600/70 pl-4'> 
-              ⏤ Billy Monger (UX/UI Designer at Monger Tech)
-            </p>
-            <p className='text-lg italic pt-5'>
-            "A very excited and enthusiastic person who elevates the energy in the room! Also concerns herself with making sure everyone is doing well."
-            </p>
-            <p className='font-Inter text-sm tracking-wide text-slate-600/70 pl-4'> 
-              ⏤ Ben Maud (Friend)
-            </p>
-          </AccordionItemPanel>
-        </AccordionItem>
-        <AccordionItem className='pt-2'>
-          <AccordionItemHeading>
-            <AccordionItemButton className='px-3 py-1 font-Roboto-Mono text-md text-slate-700 border-[1px] border-slate-200 rounded-md shadow-sm'>
-              ▾ "What are your interests?"
-            </AccordionItemButton>
-          </AccordionItemHeading>
-          <AccordionItemPanel className='px-4 py-2'>
-            <p className='font-Inter text-start text-sm tracking-wide text-slate-800 pl-4 leading-loose'> 
-            I'm a huge nerd for philosophy, music, games, tech and plants! You'll often find me exploring random topics or trying something new.
-            </p>
-          </AccordionItemPanel>
-        </AccordionItem>
-      </Accordion>
-
+    <div id='About' className='max-w-[1040px] m-auto px-5 flex flex-col md:flex-row py-20'>
       <div className='md:w-1/2'>
+        <h1 className='font-DM-Sherif-Display italic font-semibold text-4xl text-slate-900 text-start underline underline-offset-8 py-10 sm:pl-1'>
+          ABOUT
+        </h1>
+        <p className='flex text-start font-Inter tracking-wide text-slate-800 text-md p-1 leading-loose pb-5 pr-10'>
+          A passionate, driven and explorative thinker - fuelled by curiosity in discovering innovative solutions through the combination of business, design, technological and humanitarian perspectives. 
+        </p>
+        <h2 className='font-DM-Sherif-Display italic font-semibold text-3xl text-slate-700 text-start py-3'>
+          Find out more:
+        </h2>
+
+        <Accordion allowZeroExpanded className='text-start pr-10'>
+          <AccordionItem>
+            <AccordionItemHeading>
+              <AccordionItemButton className='px-3 py-1 font-Roboto-Mono text-md border-[1px] border-slate-200 rounded-md shadow-sm'>
+                ▾ "How would your friends describe you?"
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel className='px-4 py-2'>
+              <p className='text-lg italic'>
+                "Lizette is funny, hardworking and loveable. She makes a great partner in any project and always gives the task at hand 110%."
+              </p>
+              <p className='font-Inter text-sm tracking-wide text-slate-600/70 pl-4'> 
+                ⏤ Billy Monger (UX/UI Designer at Monger Tech)
+              </p>
+              <p className='text-lg italic pt-5'>
+              "A very excited and enthusiastic person who elevates the energy in the room! Also concerns herself with making sure everyone is doing well."
+              </p>
+              <p className='font-Inter text-sm tracking-wide text-slate-600/70 pl-4'> 
+                ⏤ Ben Maud (Friend)
+              </p>
+            </AccordionItemPanel>
+          </AccordionItem>
+          <AccordionItem className='pt-2'>
+            <AccordionItemHeading>
+              <AccordionItemButton className='px-3 py-1 font-Roboto-Mono text-md text-slate-700 border-[1px] border-slate-200 rounded-md shadow-sm'>
+                ▾ "What are your interests?"
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel className='px-4 py-2'>
+              <p className='font-Inter text-start text-sm tracking-wide text-slate-800 pl-4 leading-loose'> 
+                I'm a huge nerd for tech, philosophy, music, games and plants! You'll often find me exploring random topics or trying something new.
+              </p>
+            </AccordionItemPanel>
+          </AccordionItem>
+        </Accordion>
+      </div>
+
+      <div className='md:w-1/2 flex justify-end py-10'>
         <Carousel
           showStatus={false}
           showIndicators={true}
@@ -62,19 +64,22 @@ const About = () => {
           infiniteLoop={true}
           autoPlay={true}
           interval={3000}
-          className="rounded-lg overflow-hidden ml-auto"
+          className="overflow-hidden"
         >
           <div>
-            <img src="hike.jpeg" alt="Hiking" />
+            <img src="../public/images/Hiking.jpeg" alt="Hiking" />
           </div>
           <div>
-            <img src="family.jpeg" alt="Family" />
+            <img src="../public/images/Coding2.jpeg" alt="Coding2" />
           </div>
           <div>
-            <img src="music.JPG" alt="Music" />
+            <img src="../public/images/CA.jpg" alt="Coder Academy" />
           </div>
           <div>
-            <img src="coding1.JPG" alt="Coding" />
+            <img src="../public/images/Music.jpg" alt="Music" />
+          </div>
+          <div>
+            <img src="../public/images/Coding1.JPG" alt="Coding1" />
           </div>
         </Carousel>
       </div>
