cdd647b8744a3e1d2a74671d866d7d2d9423b412
Lizette Dimalanta <105244120+Lizette-Dimalanta@users.noreply.github.com>
2023-06-09 23:23:54 +1000

    Updated quote

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Quote = () => {
   return (
-    <div id='Experience' className='w-screen m-auto px-5 bg-slate-400 py-20 flex justify-center'>
+    <div id='Experience' className='w-screen m-auto px-5 bg-slate-600 py-20 flex justify-center'>
         <h1 className='max-w-[1040px] font-DM-Sherif-Display text-3xl font-semibold italic text-white'>
             "We must give ourselves permission to be who we want to be, even if we don’t have the blueprint yet." - Luvvie Ajayi Jones
         </h1>
